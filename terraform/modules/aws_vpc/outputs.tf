98fc638a9cda14eabe9bfcc23e5a88223dd25539
petrol <konevnt@gmail.com>
2021-02-10 22:35:09 +0500

    Subnets list output

--- terraform/modules/aws_vpc/outputs.tf
+++ terraform/modules/aws_vpc/outputs.tf
@@ -3,12 +3,25 @@ output "vpc_id" {
     description = "VPC ID"
 }
 
+locals {
+    public_ids_map = {
+        for subnet in aws_subnet.public_subnets:
+        subnet.tags.Name => join(", ", [subnet.id])
+    }
+    protected_ids_map = {
+        for subnet in aws_subnet.protected_subnets:
+        subnet.tags.Name => join(", ", [subnet.id])
+    }
+    public_ids_list = values(local.public_ids_map)
+    protected_ids_list = values(local.protected_ids_map)
+}
 output "public_subnets" {
-    value = aws_subnet.public_subnets
+    value = local.public_ids_list
     description = "Public subnets list"
 }
 
 output "protected_subnets" {
-    value = aws_subnet.protected_subnets
+    value = local.protected_ids_list
     description = "Protected subnets list"
 }
+
